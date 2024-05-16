f801a87d8d6ef1a4278b055c7b2e6195bb542daa
kunnor <zheng-kun@foxmail.com>
2024-05-16 16:05:50 +0800

    init(global): 🎉中文提示版

--- ah_smart_commits.js
+++ ah_smart_commits.js
@@ -47,6 +47,7 @@ var filterSubject = function(subject, disableSubjectLowerCase) {
     subject =
       subject.charAt(0).toLowerCase() + subject.slice(1, subject.length);
   }
+  // 去掉结尾的句号
   while (subject.endsWith('.')) {
     subject = subject.slice(0, subject.length - 1);
   }
@@ -81,21 +82,19 @@ const fuzzy = new fuse(choices, {
 const questions = [
   {
     type: 'autocomplete',
-    // type: 'list',
-    name: 'type',
-    message: "Select the type of change you're committing:",
+    name: 'type', // 提交类型
+    message: "选择您本次提交的更改类型:",
     choices: choices,
-    // source: (_, query) => Promise.resolve(choices),
     source: (_, query) =>  {
-      // console.log(Promise.resolve(query ? fuzzy.search(query) : choices))
+      // 搜索匹配
       return  Promise.resolve(query ? fuzzy.search(query).map(el => el.item) : choices) 
     }
   },
   {
     type: 'input',
-    name: 'scope',
+    name: 'scope', // 提交范围
     message:
-      'What is the scope of this change (e.g. component or file name): (press enter to skip)',
+      '这次变化的范围 (可填模块名或文件名): (按回车跳过)',
     // default: options.defaultScope,
     filter: function(value) {
       return options.disableScopeLowerCase
@@ -105,20 +104,20 @@ const questions = [
   },
   {
     type: 'input',
-    name: 'subject',
+    name: 'subject', // 提交简介
     message: function(answers) {
       return (
-        'Write a short, imperative tense description of the change (max ' +
+        '写一个间断的变化描述，使用祈使句 (最多 ' +
         maxSummaryLength(options, answers) +
-        ' chars):\n'
+        ' 个字符):\n'
       );
     },
     default: options.defaultSubject,
     validate: function(subject, answers) {
       var filteredSubject = filterSubject(subject, options.disableSubjectLowerCase);
       return filteredSubject.length == 0
-        ? 'subject is required'
-        : filteredSubject.length <= maxSummaryLength(options, answers)
+        ? 'subject is required' // 简介必填
+        : filteredSubject.length <= maxSummaryLength(options, answers) // 长度控制
         ? true
         : 'Subject length must be less than or equal to ' +
           maxSummaryLength(options, answers) +
@@ -127,7 +126,7 @@ const questions = [
           ' characters.';
     },
     transformer: function(subject, answers) {
-      var filteredSubject = filterSubject(subject, options.disableSubjectLowerCase);
+      var filteredSubject = filterSubject(subject, options.disableSubjectLowerCase); // 长短提示
       var color =
         filteredSubject.length <= maxSummaryLength(options, answers)
           ? chalk.green
@@ -140,40 +139,41 @@ const questions = [
   },
   {
     type: 'input',
-    name: 'body',
+    name: 'body', // 详细的提交描述，选填
     message:
-      'Provide a longer description of the change: (press enter to skip. Use imperative, present tense: “change” not “changed” nor “changes”,Includes motivation for the change and contrasts with previous behavior)\n',
+      '提供更详细的更改描述: (按回车跳过. 使用祈使句（使用“修改XXX”，而不是“修改了XXX”）,包括改变的动机和与之前的变化)\n',
     default: options.defaultBody
   },
   {
     type: 'confirm',
-    name: 'isBreaking',
-    message: 'Are there any breaking changes?',
+    name: 'isBreaking', //是否为不兼容的更改
+    message: '是否为不兼容（破坏性）的变更?(default: n)',
     default: false
   },
   {
     type: 'input',
-    name: 'breaking',
+    name: 'breaking', // 不兼容的更改必须添加Footer，详细描述更改
     message:
-      'A BREAKING CHANGE commit requires a Footer. Please enter a longer description of the commit itself:(It can be description of the change, justification and migration notes)\n',
+      'BREAKING CHANGE（破坏性/不兼容的更改）强制要求填写Footer. 请输入更详细的描述(包括变更内容、变更理由和迁移说明):\n',
     when: function(answers) {
       return answers.isBreaking;
     },
     validate: function(breakingBody, answers) {
       return (
         breakingBody.trim().length > 0 ||
-        'Footer is required for BREAKING CHANGE'
+        'Footer 对于 BREAKING CHANGE 为必填项'
       );
     }
   },
   {
     type: 'input',
-    name: 'issues',
-    message: 'Please enter the key of the closed issue on JIRA.(e.g. PORT-1233 or PORT-12, PORT-13, PORT-15):(press enter to skip)\n',
+    name: 'issues', // 如果是fix类型的提交，可以填写JIRA上的bug编号
+    message: '请输入修复BUG的JIRA/GITHUB/禅道编号，多个编号使用", "分割.(例如单个bug：“BUG-1233” ；或者多个Bug： “BUG-12, BUG-13, BUG-15”):(按回车键跳过)\n',
     when: function(answers) {
       return answers.type.name === 'fix'
     },
   }
+  // TODO 如果是revert更改，必须填写revert的提交id
 
 ]
 
